4649e77affd6949e684c4003f1d2de42a40f54a6
austinrobichaux18 <54606588+austinrobichaux18@users.noreply.github.com>
2026-06-12 18:59:33 -0500

    intial done. Ready to push to mod portal.

--- control.lua
+++ control.lua
@@ -5,8 +5,6 @@ local main = {}
 --Given a valid adjustable inserter entity, apply adjustments that will work with adjustable inserter mods.
 --Return true if an edit was made.
 local function try_adjust_inserter(entity)
-            game.print('is using try_adjust_inserter')
-
     local old_pickup_vector = {
         x = entity.pickup_position.x - entity.position.x,
         y = entity.pickup_position.y - entity.position.y
@@ -63,15 +61,12 @@ end
 ---@param player_index any
 ---@param is_orientation_invalid function Takes in a LuaEntity, and returns TRUE if the orientation is INVALID.
 local function force_orientation_condition(entity, player_index, is_orientation_invalid)
-    game.print('is using force_orientation_condition')
-
     if not is_orientation_invalid(entity) then return end --Done without issues
-
+    
     entity.rotate{by_player=player_index}
     squash_undo_actions(player_index)
 
     if not is_orientation_invalid(entity) then return end --Done without issues
-        game.print('rotated  but still bad')
     
     local true_type = (entity.type == "entity-ghost") and entity.ghost_type or entity.type
     error("Could not find an allowed orientation for an entity of type: " .. entity.prototype.name .. ", True type: " .. true_type)
@@ -122,28 +117,22 @@ function main.direction_correction(entity, player_index, skip_recheck)
         or not entity.valid
          then return end
 
-    -- --This recheck is currently needed due to a bug in 2.0.66
-    -- if not skip_recheck then 
-    --     rubia.timing_manager.wait_then_do(1, "rubia-wind-correction", {entity, player_index, true})
-    -- end
-
     local entity_type = entity.type;
     if entity.type == "entity-ghost" then 
         entity_type = entity.ghost_type end
 
-    --Requester check
-    -- investigate targeted_items_pickup and targeted_items_deliver fields from get_requester_point
-    -- local req_point = entity.get_requester_point()
-    -- if req_point and not maintain_requester_types[entity_type] then
-    --     req_point.enabled = false
+    --Requester check. Disables requester chests. Ideally would like bots not to fly south with items but idk how to do that.
+    local req_point = entity.get_requester_point()
+    if req_point and not maintain_requester_types[entity_type] then
+        req_point.enabled = false
        
-    --     local sections = entity.get_logistic_sections()
-    --     if remove_logi_section_types[entity_type] and sections then
-    --         for i = 1, sections.sections_count do
-    --             sections.remove_section(i)
-    --         end
-    --     end
-    -- end
+        local sections = entity.get_logistic_sections()
+        if remove_logi_section_types[entity_type] and sections then
+            for i = 1, sections.sections_count do
+                sections.remove_section(i)
+            end
+        end
+    end
 
     -- Only horizontal rails
     if entity_type:find("rail") then
@@ -172,6 +161,10 @@ function main.direction_correction(entity, player_index, skip_recheck)
         end 
     end
 end
+
+-- todo check with modded inserters
+-- todo try to make bots not fly south if they are carrying items
+
 
 script.on_event(defines.events.on_built_entity, function(event)
     main.direction_correction(event.entity, event.player_index)
